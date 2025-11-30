b833b581039b7a7c2181174d5a6b864f1a78110b
chanphong260913-gif <chanphong260913@gmail.com>
2025-11-30 14:21:12 +0700

    first commit

---
+++ LESSON_1/BTVN2.py
@@ -0,0 +1,9 @@
+HoTen = input("Phạm Chấn Phong: ")
+NgaySinh = input("26/09/2013: ")
+QueQuan = input("Tây Ninh: ")
+SoThich = input("Chơi Game: ")
+
+print("Họ tên: " + HoTen)
+print("Ngày sinh: " + NgaySinh)
+print("Quê quán: " + QueQuan)
+print("Sở thích: " + SoThich)
---
+++ LESSON_1/index.py
@@ -0,0 +1,12 @@
+# Nhập thông tin cơ bản -> in ra màn hình
+# Đặt biến name để lưu lại tên được nhập vào
+# Tên
+name = input("Ban Ten Gi?")
+# tuoi
+age = input("Ban Bao Nhieu Tuoi")
+# sở thích
+hobbies = input("Ban Co So Thich Gi")
+# in ra màn hình
+print("""Xin chao ban {name}, 
+      tuoi cua ban la {age} 
+      va co so thich la {hobbies}.""")
